bac584bbfc8c863f44f6a4b4f24cd48ca8570c11
myygunduz <myygunduz@gmail.com>
2021-08-25 08:18:23 +0300

    fix

--- Areas/shareLinkTable.py
+++ Areas/shareLinkTable.py
@@ -38,43 +38,43 @@ class ShareLink(QWidget):
             <tr>
                 <td>
                     <a href="https://web.facebook.com/sharer.php?t={self.message.text().replace(" ","%20")}&u={self.link.text()}&_rdc=1&_rdr" onclick="return false;">
-                        <img src="https://github.com/gayanvoice/github-active-users-monitor/raw/master/public/images/icons/facebook.svg" height="48" width="48" alt="Facebook"/>
+                        <img src="https://github.com/myygunduz/Badge-Link-Creater/blob/main/Assets/icons/facebook.svg" height="48" width="48" alt="Facebook"/>
                     </a>
                 </td>
                 <td>
                     <a href="https://www.facebook.com/dialog/send?link={self.link.text()}&app_id=291494419107518&redirect_uri={self.link.text()}" onclick="return false;">
-                        <img src="https://github.com/gayanvoice/github-active-users-monitor/raw/master/public/images/icons/facebook_messenger.svg" height="48" width="48" alt="Facebook Messenger"/>
+                        <img src="https://github.com/myygunduz/Badge-Link-Creater/blob/main/Assets/icons/messenger.svg" height="48" width="48" alt="Facebook Messenger"/>
                     </a>
                 </td>
                 <td>
                     <a href="https://twitter.com/intent/tweet?text={self.message.text().replace(" ","%20")}&url={self.link.text()}" onclick="return false;">
-                        <img src="https://github.com/gayanvoice/github-active-users-monitor/raw/master/public/images/icons/twitter.svg" height="48" width="48" alt="Twitter"/>
+                        <img src="https://github.com/myygunduz/Badge-Link-Creater/blob/main/Assets/icons/twitter.svg" height="48" width="48" alt="Twitter"/>
                     </a>
                 </td>
                 <td>
                     <a href="https://web.whatsapp.com/send?text={self.message.text().replace(" ","%20")} {self.link.text()}" onclick="return false;">
-                        <img src="https://github.com/gayanvoice/github-active-users-monitor/blob/master/public/images/icons/whatsapp.svg" height="48" width="48" alt="WhatsApp"/>
+                        <img src="https://github.com/myygunduz/Badge-Link-Creater/blob/main/Assets/icons/whatsapp.svg" height="48" width="48" alt="WhatsApp"/>
                     </a>
                 </td>
                 <td>
                     <a href="https://t.me/share/url?url={self.link.text()}&text=G{self.message.text().replace(" ","%20")}" onclick="return false;">
-                        <img src="https://github.com/gayanvoice/github-active-users-monitor/blob/master/public/images/icons/telegram.svg" height="48" width="48" alt="Telegram"/>
+                        <img src="https://github.com/myygunduz/Badge-Link-Creater/blob/main/Assets/icons/telegram.svg" height="48" width="48" alt="Telegram"/>
                     </a>
                 </td>
                 <td>
                     <a href="https://www.linkedin.com/shareArticle?title={self.message.text().replace(" ","%20")}&url={self.link.text()}" onclick="return false;">
-                        <img src="https://github.com/gayanvoice/github-active-users-monitor/blob/master/public/images/icons/linkedin.svg" height="48" width="48" alt="LinkedIn"/>
+                        <img src="https://github.com/myygunduz/Badge-Link-Creater/blob/main/Assets/icons/linkedin.svg" height="48" width="48" alt="LinkedIn"/>
                     </a>
                 </td>
                 <td>
                     <a href="https://www.reddit.com/submit?title={self.message.text().replace(" ","%20")}&url={self.link.text()}" onclick="return false;">
-                        <img src="https://github.com/gayanvoice/github-active-users-monitor/blob/master/public/images/icons/reddit.svg" height="48" width="48" alt="Reddit"/>
+                        <img src="https://github.com/myygunduz/Badge-Link-Creater/blob/main/Assets/icons/reddit.svg" height="48" width="48" alt="Reddit"/>
                     </a>
                 </td>
             </tr>
         </table>"""
         self.table = QWebEngineView()
-        self.table.setHtml(self.htmlCode)
+        self.table.setHtml(self.htmlCode.strip())
         
         self.mainLayout.addWidget(self.table)
         self.copyLayout.lineEdit.setText(self.htmlCode.replace('onclick="return false;"',""))
